b6646b759651d2e6a138f4b6e90e33bde5b8bbf2
Vasya <cola100@yandex.ru>
2017-08-21 15:46:14 +0000

    prettify

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
 
+import "./Root.scss";
+
 import Autocomplete from "../components/Autocomplete";
 
-export default () => (
-    <div style={{ display: "flex", justifyContent: "space-around" }}>
-        <div style={{ width: "310px"}}>
-            <Autocomplete placeholder="Введите или выберите из списка" url="/searchcity" arrow={true} queryValue="City"/>
-        </div>
-        <div style={{ width: "310px"}}>
-            <Autocomplete placeholder="Начните вводить код или название" url="/searchcity" arrow={false}  queryValue="City"/>
-        </div>
-    </div>
-);
+export default class Root extends Component {
+    render() {
+        return (
+            <div className="flex-items">
+                <div>
+                    <h4>Автокомплит со стрелкой</h4>
+                    <span className="city">Город</span>
+                    <Autocomplete
+                        arrow={true}
+                        url="/searchcity"
+                        queryValue="City"
+                        width={310}
+                        placeholder="Введите или выберите из списка"
+                    />
+                </div>
+                <div>
+                    <h4>Автокомплит без стрелки</h4>
+                    <span className="city">Город</span>
+                    <Autocomplete
+                        arrow={false}
+                        url="/searchcity"
+                        queryValue="City"
+                        width={310}
+                        placeholder="Начните вводить код или название"
+                    />
+                </div>
+            </div>
+        );
+    }
+}
