2ce475841bb02815bc0c79f29c6c5bbec3a7d915
Nikita Kulikov <cola100@yandex.ru>
2017-10-13 00:15:46 +0500

    Added stories for Autocomplete and data file for it

--- src/components/Autocomplete/__tests__/Autocomplete.stories.js
+++ src/components/Autocomplete/__tests__/Autocomplete.stories.js
@@ -1,6 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
 import { storiesOf } from "@storybook/react";
 import Autocomplete from "../Autocomplete";
 
+let data = require("./kladr.json");
+
+let delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+/**
+ * Simulate request delay
+ */
+function simulateFetch(value) {
+    return delay(Math.floor(Math.random() * 2000))
+        .then(() => filterByContain(value, data))
+}
+
+/**
+ * Simulate server-side json api
+ */
+let filterByContain = (value, data) => {
+    return data.filter(item => item.City.toLowerCase().indexOf(value.toLowerCase()) == 0);
+}
+
+let filterByAlphabet = data => {
+    return data.sort((a, b) => {
+        let cityA = a.City.toLowerCase();
+        let cityB = b.City.toLowerCase();
+
+        return cityA < cityB ? -1 : cityA > cityB ? 1 : 0;
+    });
+}
+
+/*eslint-disable react/prop-types */
+class Demo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: "",
+            data: null
+        }
+    }
+
+    render() {
+        return (
+            <Autocomplete
+                value={this.state.value}
+                data={this.state.data}
+                itemsCount={5}
+                structure={this.props.structure}
+                placeholder="Введите значение"
+                onChange={this.handleChange}
+                width={150}
+            />
+        );
+    }
+
+    handleChange = async e => {
+        let { value } = e.target;
+
+        if(!value) {
+            return this.setState({ value: "", data: null });
+        }
+
+        this.setState({ value });
+
+        let data = await simulateFetch(value);
+
+        this.setState({ data: filterByAlphabet(data) });
+    }
+}
+
 storiesOf("Autocomplete", module)
-    .add("without props", () => <Autocomplete />)
+    .add("plain structure in list", () => <Demo structure={["City"]} />)
+    .add("composite structure in list", () => <Demo structure={["Id", "City"]} />)
