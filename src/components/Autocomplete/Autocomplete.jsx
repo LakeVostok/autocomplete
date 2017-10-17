5fa50517db1b73f04c3c35fd19ee4228a5e267c8
Nikita Kulikov <cola100@yandex.ru>
2017-10-17 22:50:52 +0500

    New way to display data

--- src/components/Autocomplete/Autocomplete.jsx
+++ src/components/Autocomplete/Autocomplete.jsx
@@ -10,7 +10,20 @@ import Loader from "../Loader";
 export default class Autocomplete extends Component {
     static propTypes = {
         placeholder: PropTypes.string,
-        width: PropTypes.number
+        width: PropTypes.number,
+        /**
+         * Url to fetch or array of data
+         */
+        data: PropTypes.oneOfType([PropTypes.string, PropTypes.array]).isRequired,
+        /**
+         * The key in data by which will be filtering occur
+         */
+        queryValue: PropTypes.string.isRequired,
+        itemsCount: PropTypes.number
+    }
+
+    static defaultProps = {
+        itemsCount: 6
     }
 
     constructor(props) {
@@ -18,7 +31,31 @@ export default class Autocomplete extends Component {
 
         this.state = {
             value: "",
-            opened: false
+            opened: false,
+            data: null,
+        }
+
+        this.throttledUpdate = throttle(this.updateData, 5000);
+    }
+
+    componentWillMount() {
+        let { data } = this.props;
+
+        switch(getType(data)) {
+        case "Array":
+            this.getData = (value, queryValue, itemsCount) => {
+                return Promise.resolve(data)
+                    .then(data => filterItems(data, value, queryValue, itemsCount));
+            }
+            break;
+        case "String":
+            this.getData = (value, queryValue, itemsCount) => {
+                return fetch(`${data}?${queryValue}=${value}`)
+                    .then(res => res.json())
+                    .then(data => filterItems(data, value, queryValue, itemsCount))
+            }
+            break;
+        default: throw new Error(`Wrong data type ${data}`);
         }
     }
 
@@ -47,7 +84,7 @@ export default class Autocomplete extends Component {
                         //highlighted={this.state.highlightedIndex}
                         ref={component => this.list = component}
                     >
-                        <ListItem />
+                        <ListElements data={this.state.data} queryValue={this.props.queryValue} />
                     </List>
                     <NotFound />
                 </Dropdown>
@@ -55,7 +92,9 @@ export default class Autocomplete extends Component {
         );
     }
 
-    handleChange = value => this.setState({ value })
+    handleChange = value => {
+        this.setState({ value, data: null }, this.throttledUpdate);
+    }
 
     handleFocus = () => this.setState({ opened: true })
 
@@ -78,7 +117,47 @@ export default class Autocomplete extends Component {
         }
     }
 
+    updateData = async () => {
+        if(!this.state.value) return;
+
+        let { queryValue, itemsCount } = this.props;
+
+        let data = await this.getData(this.state.value, queryValue, itemsCount);
+
+        this.setState({ data });
+    }
+
     refNode = node => this.input = node
+}
+
+function filterItems(data, value, queryValue, itemsCount) {
+    let isEqual = item => item[queryValue]
+        .toLowerCase()
+        .indexOf(value.toLowerCase()) == 0;
+
+    let compare = (a, b) => {
+        let itemA = a[queryValue].toLowerCase();
+        let itemB = b[queryValue].toLowerCase();
+
+        return itemA < itemB ? -1 : itemA > itemB ? 1 : 0;
+    };
+
+    return data.filter(isEqual).sort(compare).slice(0, itemsCount);
+}
+
+function ListElements({data, queryValue}) {
+    return data && data.length ? (
+        data.map((item, index) => (
+            <ListItem dataset={item} key={index}>
+                { item[queryValue] }
+            </ListItem>)
+        )
+    ) : null;
+}
+
+ListElements.propTypes = {
+    data: PropTypes.array,
+    queryValue: PropTypes.string
 }
 
 function Loading({loading}) {
@@ -102,3 +181,32 @@ function NotFound({display}) {
 NotFound.propTypes = {
     display: PropTypes.bool
 };
+
+function getType(data) {
+    return ({}).toString.call(data).slice(8, -1);
+}
+
+function throttle(callback, delay) {
+    let isThrottled = false, args, context;
+
+    function wrapper() {
+        if (isThrottled) {
+            args = arguments;
+            context = this;
+            return;
+        }
+
+        isThrottled = true;
+        callback.apply(this, arguments);
+
+        setTimeout(() => {
+            isThrottled = false;
+            if (args) {
+                wrapper.apply(context, args);
+                args = context = null;
+            }
+        }, delay);
+    }
+
+    return wrapper;
+}
--- src/components/Autocomplete/__tests__/Autocomplete.stories.js
+++ src/components/Autocomplete/__tests__/Autocomplete.stories.js
@@ -4,75 +4,21 @@ import Autocomplete from "../Autocomplete";
 
 let data = require("./kladr.json");
 
-let delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-/**
- * Simulate request delay
- */
-function simulateFetch(value) {
-    return delay(Math.floor(Math.random() * 5000))
-        .then(() => filterByContain(value, data))
-}
-
-/**
- * Simulate server-side json api
- */
-let filterByContain = (value, data) => {
-    return data.filter(item => item.City.toLowerCase().indexOf(value.toLowerCase()) == 0);
-}
-
-let filterByAlphabet = data => {
-    return data.sort((a, b) => {
-        let cityA = a.City.toLowerCase();
-        let cityB = b.City.toLowerCase();
-
-        return cityA < cityB ? -1 : cityA > cityB ? 1 : 0;
-    });
-}
-
 /*eslint-disable react/prop-types */
 class Demo extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            value: "",
-            data: null,
-            selected: null
-        }
-    }
-
     render() {
         return (
             <Autocomplete
-                value={this.state.value}
-                queryValue="City"
-                data={this.state.data}
-                itemsCount={5}
-                structure={this.props.structure}
                 placeholder="Введите значение"
-                onChange={this.handleChange}
-                width={150}
-                onSelect={this.handleSelect}
+                width={300}
+                data={this.props.data}
+                queryValue={this.props.queryValue}
+                itemsCount={5}
             />
         );
     }
-
-    handleChange = async value => {
-        if(!value) {
-            return this.setState({ value: "", data: null });
-        }
-
-        this.setState({ value });
-
-        let data = await simulateFetch(value);
-
-        this.setState({ data: filterByAlphabet(data) });
-    }
-
-    handleSelect = selected => this.setState({ selected }) 
 }
 
 storiesOf("Autocomplete", module)
-    .add("plain structure in list", () => <Demo structure={["City"]} />)
-    .add("composite structure in list", () => <Demo structure={["Id", "City"]} />)
+    .add("Static data", () => <Demo data={data} queryValue="City" />)
+    .add("Dynamically loaded data", () => <Demo data="http://localhost:8081/searchcity" queryValue="City" />)
