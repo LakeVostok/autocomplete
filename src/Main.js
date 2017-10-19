5c46d160b79009b67e0b820f64f5f04b310bb44c
Nikita Kulikov <cola100@yandex.ru>
2017-10-20 02:51:02 +0500

    Updated demo

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
 import Autocomplete from "./components/Autocomplete";
+import "./Main.scss";
 
 const data = require("./kladr.json");
 
 export default class Main extends Component {
     render() {
         return (
-            <div style={{ display: "flex", justifyContent: "center", marginTop: "100" }}>
+            <div>
+                <div className="selected">
+                    Выбрано:
+                    <pre> {this.selected}</pre>
+                </div>
+                <hr className="divider" />
                 <Autocomplete
                     placeholder="Начните вводить название города"
                     data={data}
                     queryValue="City"
                     itemsCount={5}
                     width={300}
+                    onSelect={this.handleSelect}
                 />
             </div>
         );
+    }
+
+    handleSelect = selected => {
+        this.selected = JSON.stringify(selected);
+        this.forceUpdate();
     }
 }
